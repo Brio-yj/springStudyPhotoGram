11cba8e3dcc7bfc59d68aaad4a05705540762636
조용진 <101401582+Brio-yj@users.noreply.github.com>
2024-05-10 15:19:37 +0900

    .

---
+++ src/main/java/com/example/metaphotogram/web/HttpController.java
@@ -0,0 +1,29 @@
+package com.example.metaphotogram.web;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+
+public class HttpController {
+    @GetMapping("/get")
+    public String get() {
+        return "<h1>get요청됨</h1>";
+    }
+
+    @PostMapping("/post")
+    public String post() {
+        return "post요청됨";
+    }
+
+    @PutMapping("/put")
+    public String put() {
+        return "put요청됨";
+
+    }
+
+    @DeleteMapping("/delete")
+    public String delete() {
+        return "delete요청됨";
+    }
+}
---
+++ src/test/java/com/example/metaphotogram/MetaPhotogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metaphotogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaPhotogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
